65f9f34cacb800d9734bd8ddb1bbbf3788cf71d6
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-12-11 17:30:37 +0000

    Liste des districts

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -50,7 +50,9 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }/**
+    }
+	
+	/**
  * @Route("/change-password/", name="app_password_change")
  */
 	public function password(Request $request)
