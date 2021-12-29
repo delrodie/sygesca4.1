b5c5dff7a9b64f00e373b43004f7912277eadcba
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-12-29 19:13:17 +0000

    Sygesca Gestion objectif updated

--- src/Controller/Backend/SygescaTelemetrieController.php
+++ src/Controller/Backend/SygescaTelemetrieController.php
@@ -24,9 +24,21 @@ class SygescaTelemetrieController extends AbstractController
     public function index(): Response
     {
 		$annee = $this->_cotisation->annee();
+		
+		$objectifs = $this->_cotisation->listeObjectifs();
+		$total_inscrit = 0; $total_objectif=0;
+		foreach ($objectifs as $objectif){
+			$total_inscrit = $total_inscrit + $objectif['inscrit'];
+			$total_objectif = $total_objectif + $objectif['valeur'];
+		}
+		$total_pourcentage = round($total_inscrit*100 / $total_objectif, 2);
+		
         return $this->render('sygesca_telemetrie/index.html.twig', [
             'regions' => $this->_cotisation->statistiquesRegion($annee),
-	        'objectifs' => $this->_cotisation->listeObjectifs()
+	        'objectifs' => $objectifs,
+	        'total_pourcent' => $total_pourcentage,
+	        'total_inscrit' => $total_inscrit,
+	        'total_objectif' => $total_objectif
         ]);
     }
 }
